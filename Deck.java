335c8d73a712a2c5fe9b5a65ede55831a253add6
fangfurtail <kaan.bilgili@std.izmirekonomi.edu.tr>
2023-12-16 15:00:12 +0300

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Deck {
+	private final String[] Colour = {"RED", "YELLOW","GREEN","BLUE"};
+	private final String[] Number = {"1", "2", "3", "4", "5", "6", "7", "8", "9","10"};
+	private int [] gameDeck ;
+	Scanner scan = new Scanner(System.in);
+	Random rd = new Random();
+	private final 
+			}
+	
+	
+	
+	public Deck() {
+		
+		
+	}
+
+
