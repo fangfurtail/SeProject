1b82cba011a0c58ef484cce10db4c2c3413ccae7
fangfurtail <kaan.bilgili@std.izmirekonomi.edu.tr>
2023-12-18 21:14:52 +0300

    Created Board.java
    Its just a start for now

---
+++ Board.java
@@ -0,0 +1,12 @@
+public class Board{
+	
+	public Card[] boardCardsForUser = new Card[10]; // created a board for the game ; for each
+	public Card[] boardCardsForCPU = new Card[10];  // an array with 10 space is more than enough but I ll keep it with 10;
+	// I dont need to try to hide them , it s cpu's deck 
+	
+	
+	Board(){
+
+		// default constructor in case 
+	}
+	
