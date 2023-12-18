204220754a1ec2bfcd58fd0792a39fc66eb68f06
fangfurtail <kaan.bilgili@std.izmirekonomi.edu.tr>
2023-12-18 22:10:01 +0300

    adding users decks and started to create their main and game decks

--- Deck.java
+++ Deck.java
@@ -6,9 +6,16 @@ public class Deck {
 	private final String[] Number = {"1", "2", "3", "4", "5", "6", "7", "8", "9","10"};
 	private final String[] Signs = { "+" , "-"};
 	
-	private Card temporaryCard;
-	private int deckCreaterIndex= 0; 
-	private Card[] deck ;
+	private Card temporaryCard;			  // will be used to shuffle the cards
+	private int deckCreaterIndex= 0;      // will be used to create a desk
+	private Card[] Deck ;		// since now I have a deck , Cards can store 3 values
+	private Card[] UserDeckNotMain ; // I need to get 10 cards for the user to select 4 of them.
+	private Card[] UserDeckMain  ;	   // Selected 4 
+	private Card[] CpuDeckNotMain ;  
+	private Card[] CpuDeckMain  ;  
+	
+	
+	// I already assigned !!!!  boardCardsForUser and boardCardsForCPU in Board class.
 	Scanner scan = new Scanner(System.in);
 	Random rd = new Random();
 	
@@ -17,11 +24,12 @@ public class Deck {
 	
 	
 	public Deck() { //// With this I succesfully created a game deck , each has a color, number value and a sign
-		deck = new Card [40];
-		
+		Deck = new Card [40];
+		UserDeckMain = new Card [4];
+		UserDeckNotMain = new Card[10];
 		for(int t = 0 ; t < Colour.length; t++){
 			for (int n = 0 ; n < Number.length ; n++){
-				deck[deckCreaterIndex]=new Card(Colour[t] , Number [n], "+"); 
+				Deck[deckCreaterIndex]=new Card(Colour[t] , Number [n], "+");  // all signs must be positive in the main deck.
 				deckCreaterIndex++;
 		
 			}
@@ -32,19 +40,44 @@ public class Deck {
 	public void shuffle(){ 
 			for( int i = 0 ; i < 40 ; i++){				// looping through all cards in the deck.
 				int randIndex = rd.nextInt(40);			// generating a random number to replace 
-				temporaryCard = deck[randIndex];		// generated temp card to swap values
-				deck[randIndex] = deck[i];
-				deck[i] = temporaryCard;
+				temporaryCard = Deck[randIndex];		// generated temp card to swap values
+				Deck[randIndex] = Deck[i];
+				Deck[i] = temporaryCard;
 			}
 	}
-	public static void main(String[] args){  // checking if it works, yes it does work. 
-		Deck deckInstance = new Deck();
-		deckInstance.shuffle();
-		for (int t = 0; t < deckInstance.deck.length; t++) {
-        System.out.println(deckInstance.deck[t].wholeCardToString());
+	
+	public void giveCard ( ){                          // giving first 5 bottom cards to user 
+		for ( int i = 0 ; i < 5 ; i++){
+			UserDeckNotMain[i] = Deck[39-i];
 		}
 	}
-	}
+	
+	
+	
+
+	
+	 public void printUserDeckNotMain() {
+        System.out.println("UserDeckNotMain in icindekiler:");
+        for (Card card : UserDeckNotMain) {
+            System.out.println(card.wholeCardToString());
+        }
+    }
+	 public static void main(String[] args) {
+        Deck deckInstance = new Deck();
+        deckInstance.shuffle();
+        deckInstance.giveCard();
+
+        // Print UserDeckNotMain
+        deckInstance.printUserDeckNotMain();
+	 }
+}
+	
+	
+	
+	
+	
+	
+	
 	/*
 	String tempValue =" " ;
 String [] arr = {"a","b","c","d","e"};
