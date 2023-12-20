6d506e2b3c2c92f481461b9067dbb7cd1759bafb
fangfurtail <kaan.bilgili@std.izmirekonomi.edu.tr>
2023-12-20 23:26:33 +0300

    adding some features to board

--- Board.java
+++ Board.java
@@ -12,32 +12,67 @@ public class Board{
 	}
 	
 	public String game(){
-		int scoreCpu = 0;
-		int scoreUser = 0 ;
 		
 		
 		
 		
-		//    !!!   bunları user değil cpu yapacaksın!!!    
+		//        
 		
 		System.out.println("                     CPU") ;
-		int cpuHasThatMuchCardOnDeck = 3;
-		int cpuHasThatMuchCardOnHand = BoardInstance.getCpuDeckMain().length;
-		for (int i = 0 ; i < cpuHasThatMuchCardOnHand ; i++){
+		
+		
+		
+		
+		int cpuHasThatMuchCardOnHand = BoardInstance.getCpuDeckMain().length;		// It prints out the Cards which are hidden
+		for (int i = 0 ; i < cpuHasThatMuchCardOnHand ; i++){                          
 			System.out.print("       X  ");
 		}
 		System.out.println("           ");
 		System.out.println("           ");
-		for(int i = 0 ; i < cpuHasThatMuchCardOnDeck ; i ++){ 
+		
+		
+		
+		// bunu sonra method olarak eklicem          // 
+		
+	
+		// BOARD PRİNTER AND DECK SCORE CALCULATOR FOR CPU
+		int scoreCpu = 0;
+		int cpuHasThatMuchCardOnBoard = 3;    		// kart attıkca artacak 
+		for(int i = 0 ; i < cpuHasThatMuchCardOnBoard ; i ++){ 
 			if (BoardInstance.getCpuDeckMainSigns(i).equals("+")){
 				System.out.print(BoardInstance.getCpuDeckMainSigns(i) + "" +BoardInstance.getCpuDeckMainNumber(i)+ " " + BoardInstance.getCpuDeckMainColour(i) + "    ");
 				scoreCpu = scoreCpu + BoardInstance.getCpuDeckMainNumber(i);
-			}else {
+			}else if (BoardInstance.getCpuDeckMainSigns(i).equals("-")){
 				System.out.print(BoardInstance.getCpuDeckMainSigns(i) + "" +BoardInstance.getCpuDeckMainNumber(i)+ " " + BoardInstance.getCpuDeckMainColour(i) + "    ");
 				scoreCpu = scoreCpu - BoardInstance.getCpuDeckMainNumber(i);
-			}	
+			}else if (BoardInstance.getCpuDeckMainSigns(i).equals("flip")){
+				System.out.print("    +/-    " );
+			}else {
+				System.out.print("   x2    " );
+			}
 		}
-		System.out.println(scoreCpu);
+		System.out.println( " Computers Current Score  is " + scoreCpu);
+		
+		
+		 // BOARD PRİNTER AND DECK SCORE CALCULATOR FOR USER
+		int scoreUser = 0 ;
+		int userHasThatMuchCardOnBoard= 3;  		//  kart ttıkca artacak
+		for(int i = 0 ; i < userHasThatMuchCardOnBoard ; i ++){ 
+			if (BoardInstance.getUserDeckMainSigns(i).equals("+")){
+				System.out.print(BoardInstance.getUserDeckMainSigns(i) + "" +BoardInstance.getUserDeckMainNumber(i)+ " " + BoardInstance.getUserDeckMainColour(i) + "    ");
+				scoreUser = scoreUser + BoardInstance.getUserDeckMainNumber(i);
+			}else if (BoardInstance.getUserDeckMainSigns(i).equals("-")){
+				System.out.print(BoardInstance.getUserDeckMainSigns(i) + "" +BoardInstance.getUserDeckMainNumber(i)+ " " + BoardInstance.getUserDeckMainColour(i) + "    ");
+				scoreUser = scoreUser - BoardInstance.getUserDeckMainNumber(i);
+			}else if (BoardInstance.getUserDeckMainSigns(i).equals("flip")){
+				System.out.print("  +/-  " );
+			}else {
+				System.out.print(" x2 " );	
+			}
+		}	
+			
+		System.out.println( " User's Current Score  is " + scoreUser +   "   "  );
+		
 		
 		
 		
