9601bbd9bdc69aeeaea0fed550a5c75ab67b6f14
fangfurtail <kaan.bilgili@std.izmirekonomi.edu.tr>
2023-12-24 15:45:11 +0300

    FINAL MAIN CLASS

--- Main.java
+++ Main.java
@@ -1,36 +1,47 @@
 public class Main{
 	public static void main(String[] args) {
-		Board main = new Board();
-        main.BoardInstance.shuffle();
-
-        // Print the entire deck
-       
-
-        // Print UserDeckNotMain
-        main.BoardInstance.giveCard(); //  gives card  and sets firt and last 5 value to null in main deck
-		
-		//main.BoardInstance.printDeck();	
-        //main.BoardInstance.printUserDeckNotMain();
-		//main.BoardInstance.printCpuDeckNotMain();
-		//deckInstance.printTopUpCards();
-		
-		//main.BoardInstance.printCpuDeckMain();
-		
-		//System.out.println(main.boardReaderUser());  // bu default il raundu dağıtıyor
-		//System.out.println(main.boardReaderCpu());
-		//main.BoardInstance.printUserDeckMain();
-		System.out.println(main.board());
-		System.out.println(main.game());
-		
-		//System.out.println(main.game());
-		//System.out.println(main.game());
-		//main.BoardInstance.printDeck();
-		//System.out.println(main.boardReaderUser());
-		//System.out.println(main.boardReaderCpu());
-		//System.out.println(main.method());
-		//System.out.println(main.BoardInstance.getUserDeckMain(2));
-		//System.out.println(main.game());
-		
+			Board main = new Board();
+			
+			/*
+			 Print UserDeckNotMain
+			main.BoardInstance.printDeck();	
+			main.BoardInstance.printUserDeckNotMain();
+			main.BoardInstance.printCpuDeckNotMain();
+			deckInstance.printTopUpCards();
+			main.BoardInstance.printCpuDeckMain();
+			System.out.println(main.boardReaderUser());  // bu default il raundu dağıtıyor
+			System.out.println(main.boardReaderCpu());
+			main.BoardInstance.printUserDeckMain();
+			
+			
+			
+		  	     	       ^
+			             /  \
+			               |
+			               |
+			               |
+			               |
+			 YOU CAN REACH ALL TOP VALUES AND PRINT THEM FROM HERE 
+			
+			*/
+			
+			
+			
+			
+			
+			
+			
+			
+			
+							// GAME STARTS 
+							main.BoardInstance.shuffle();
+							main.BoardInstance.giveCard(); //  gives card  and sets firt and last 5 value to null in main deck
+							System.out.println(main.board());
+							System.out.println(main.game());
+						
+			
+				
+	
 		
 		
 	}
